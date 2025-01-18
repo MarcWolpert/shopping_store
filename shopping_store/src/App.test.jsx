69020fb31d5884c85b0ed4822a8c197b1e5dc96e
Marc Wolpert <mewolpert@gmail.com>
2025-01-17 16:13:34 -0800

    test:Adding items on product page accurately reflects cart

--- shopping_store/src/App.test.jsx
+++ shopping_store/src/App.test.jsx
@@ -83,10 +83,46 @@ describe('Home component', () => {
 	});
 
 	//Test: Clicking on the product cart button should add to cart
-	//Steps: Click on button,
-	//click on the cart link,
-	// check if the product is in the cart and right amount matching that
-	//of the red dot in the upper right hand corner
+	it('Clicking on the product cart button should have the same number of items in the cart as there are red bubble counts for that specific item', () => {
+		render(
+			<MemoryRouter>
+				<App />
+			</MemoryRouter>,
+		);
+		const link = screen.getByRole('link', { name: 'Products' });
+		userEvent.click(link);
+
+		//wait 5 seconds for the images to load
+		setTimeout(() => {
+			const images = screen.getAllByRole('img');
+			expect(images.length).toBeGreaterThan(12);
+			//it isn't a button it's an img tag
+			const regex = /alt={`Add [\s\S]* to cart/;
+			const addToCartButtonImg = screen.getAllByRole('img', { name: regex })[0];
+			userEvent.click(addToCartButtonImg);
+			console.log('This is the button: ', addToCartButtonImg);
+
+			//wait for the red bubble to appear
+			setTimeout(() => {
+				const redBubble = screen.getAllByRole('p', { name: '1' })[0];
+				expect(redBubble).toHaveTextContent('1');
+				//do it again to check if it increments
+				userEvent.click(addToCartButtonImg);
+				setTimeout(() => {
+					const redBubble = screen.getAllByRole('p', { name: '2' })[0];
+					expect(redBubble).toHaveTextContent('2');
+					//go to the cart page
+					const cartLink = screen.getByRole('link', { name: 'cartLink' });
+					userEvent.click(cartLink);
+					//structure is cartItem -> (cartItemPicture + details ->(p + p + p))
+					//the p tag that is 3rd is the quantity
+					const cartItems = screen.getAllByRole('div', { name: 'cartItem' });
+					const cartItemQuantity = cartItems[0].children[1].children[2];
+					expect(cartItemQuantity).toHaveTextContent('2');
+				}, 2000);
+			}, 2000);
+		}, 5000);
+	});
 
 	//Test: Decrementing the counter on the cart page for a specific item
 	//should decrement the red bubble on the product on the products page
