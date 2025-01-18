2e207b013b9a0b513e14720b92b4f6a34c15c91a
Marc Wolpert <mewolpert@gmail.com>
2025-01-17 16:17:31 -0800

    test: Decrementing on cart page reflects product page

--- shopping_store/src/App.test.jsx
+++ shopping_store/src/App.test.jsx
@@ -123,9 +123,60 @@ describe('Home component', () => {
 			}, 2000);
 		}, 5000);
 	});
-
 	//Test: Decrementing the counter on the cart page for a specific item
 	//should decrement the red bubble on the product on the products page
+	it('Decrementing the counter on the cart page for a specific item should decrement the red bubble on the product on the products page', () => {
+		render(
+			<MemoryRouter>
+				<App />
+			</MemoryRouter>,
+		);
+		const link = screen.getByRole('link', { name: 'Products' });
+		userEvent.click(link);
+
+		//wait 5 seconds for the images to load
+		setTimeout(() => {
+			const images = screen.getAllByRole('img');
+			expect(images.length).toBeGreaterThan(12);
+			//it isn't a button it's an img tag
+			const regex = /alt={`Add [\s\S]* to cart/;
+			const addToCartButtonImg = screen.getAllByRole('img', { name: regex })[0];
+			userEvent.click(addToCartButtonImg);
+			console.log('This is the button: ', addToCartButtonImg);
+
+			//wait for the red bubble to appear
+			setTimeout(() => {
+				const redBubble = screen.getAllByRole('p', { name: '1' })[0];
+				expect(redBubble).toHaveTextContent('1');
+				//do it again to check if it increments
+				userEvent.click(addToCartButtonImg);
+				setTimeout(() => {
+					const redBubble = screen.getAllByRole('p', { name: '2' })[0];
+					expect(redBubble).toHaveTextContent('2');
+					//go to the cart page
+					const cartLink = screen.getByRole('link', { name: 'cartLink' });
+					userEvent.click(cartLink);
+					//structure is cartItem -> (cartItemPicture + details ->(p + p + p))
+					//the p tag that is 3rd is the quantity
+					const cartItems = screen.getAllByRole('div', { name: 'cartItem' });
+					const cartItemQuantity = cartItems[0].children[1].children[2];
+					expect(cartItemQuantity).toHaveTextContent('2');
+					//decrement the quantity
+					const decrementButton = cartItems[0].children[1].children[2].children[1];
+					userEvent.click(decrementButton);
+					expect(cartItemQuantity).toHaveTextContent('1');
+					//go back to the products page
+					const productsLink = screen.getByRole('link', { name: 'Products' });
+					userEvent.click(productsLink);
+					setTimeout(() => {
+						//check if the red bubble is decremented
+						const redBubble = screen.getAllByRole('p', { name: '1' })[0];
+						expect(redBubble).toHaveTextContent('1');
+					}, 2000);
+				}, 2000);
+			}, 2000);
+		}, 5000);
+	});
 
 	//Test: Home Page picture should change once every 7 seconds
 });
